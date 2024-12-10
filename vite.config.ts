fe078bcf6673ab8a3e19856819d4f9f44ed3f7ad
mahaseth <yogeshmahaseth07@gmail.com>
2024-12-11 00:43:32 +0545

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,47 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',  // Automatically update the service worker
+      manifest: {
+        name: 'Expense tracker',
+        short_name: 'Exp tracker',
+        description: 'Your app description',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icons/expense-tracker-icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/expense-tracker-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        "screenshots": [
+            {
+              "src": "screenshots/expense-tracker-wide.png",
+              "sizes": "1024x585",
+              "type": "image/png",
+              "form_factor": "wide"
+            },
+            {
+              "src": "screenshots/expense-tracker-portrait.png",
+              "sizes": "512x1024",
+              "type": "image/png",
+              "form_factor": "narrow"
+            }
+          ],
+          "start_url": "/",
+          "display": "standalone",
+          "background_color": "#ffffff",
+          },
+    }),
+  ],
+});
